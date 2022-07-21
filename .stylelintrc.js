b08db75b22882c26884b37f49857800708f64f07
Lyove <lyove@outlook.com>
2022-07-21 16:56:35 +0800

    Create .stylelintrc.js

---
+++ .stylelintrc.js
@@ -0,0 +1,10 @@
+/**
+ * 官方文档：https://stylelint.io/
+ */
+module.exports = {
+  extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
+  plugins: [],
+  rules: {
+    'string-quotes': 'double'
+  }
+};
